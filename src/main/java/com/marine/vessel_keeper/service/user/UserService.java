3ea86041d8c8ddb9ca2f2539dea0a4a593c1b1f5
wtypmah24 <wtypmah24@gmail.com>
2024-03-18 13:39:34 +0100

    Test commit-3

--- src/main/java/com/marine/vessel_keeper/service/user/UserService.java
+++ src/main/java/com/marine/vessel_keeper/service/user/UserService.java
@@ -6,7 +6,6 @@ import com.marine.vessel_keeper.exception.WrongCandidateException;
 import com.marine.vessel_keeper.mapper.UserMapper;
 import com.marine.vessel_keeper.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
